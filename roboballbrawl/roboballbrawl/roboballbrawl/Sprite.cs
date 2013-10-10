21969258e93c31f0d610706fe2123fa51e763db2
jamcnary <jamesmcnary@gmail.com>
2013-10-09 23:17:12 -0500

    Initial Setup with Visual Studio

---
+++ roboballbrawl/roboballbrawl/roboballbrawl/Sprite.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace roboballbrawl
+{
+    class Sprite
+    {
+        Texture2D t2dTexture;
+        float fFrameRate = 0.02f;
+        float fElapsed = 0.0f;
+
+        int iFrameOffsetX = 0;
+        int iFrameOffsetY = 0;
+        int iFrameWidth = 32;
+        int iFrameHeight = 32;
+
+        int iFrameCount = 1;
+        int iCurrentFrame = 0;
+        int iScreenX = 0;
+        int iScreenY = 0;
+
+        bool bAnimating = true;
+
+        public int X
+        {
+            get { return iScreenX; }
+            set { iScreenX = value; }
+        }
+
+        public int Y
+        {
+            get { return iScreenY; }
+            set { iScreenY = value; }
+        }
+
+        public int Frame
+        {
+            get { return iCurrentFrame; }
+            set { iCurrentFrame = (int)MathHelper.Clamp(value, 0, iFrameCount); }
+        }
+
+        public float FrameLength
+        {
+            get { return fFrameRate; }
+            set { fFrameRate = (float)Math.Max(value, 0f); }
+        }
+
+        public bool IsAnimating
+        {
+            get { return bAnimating; }
+            set { bAnimating = value; }
+        }
+
+    }
+}
